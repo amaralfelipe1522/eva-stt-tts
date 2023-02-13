90a6935d9a8203640153517a900b5bde54504082
Felipe Amaral <amaral.felipe@live.com>
2023-02-13 14:52:40 -0300

    Ajustes de novas dependencias no DockerFile

--- index.py
+++ index.py
@@ -6,28 +6,23 @@ maquina = pyttsx3.init()
 
 print('EVA iniciada')
 
-def iniciarComando():
-    try:
-        with sr.Microphone() as source:
-            print('Escutando...')
-            voz = audio.listen(source)
+try:
+    with sr.Microphone() as source:
+        print('Escutando...')
+        voz = audio.listen(source)
 
-            comando = audio.recognize_google(voz, language='pt-BR')
-            comando = comando.lower()
+        comando = audio.recognize_google(voz, language='pt-BR')
+        comando = comando.lower()
 
-            if 'eva' in comando:
-                comando = comando.replace('eva', '', 1)
-                maquina.say(comando)
-                maquina.runAndWait()
-            
-            if 'erva' in comando:
-                comando = comando.replace('erva', '', 1)
-                maquina.say(comando)
-                maquina.runAndWait()
+        if 'eva' in comando:
+            comando = comando.replace('eva', '', 1)
+            maquina.say(comando)
+            maquina.runAndWait()
+        
+        if 'erva' in comando:
+            comando = comando.replace('erva', '', 1)
+            maquina.say(comando)
+            maquina.runAndWait()
 
-    except:
-        print('Audio não foi capturado...')
-
-    return comando
-
-iniciarComando()
+except:
+    print('Audio não foi capturado...')
