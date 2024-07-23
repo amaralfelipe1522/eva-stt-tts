7b4a531640328b0feed77d0847c8fcefa1b7365d
de Souza <feee@gft.com>
2024-07-23 09:44:16 -0300

    fix: change stt lib and adjust captured voice

--- README.md
+++ README.md
@@ -23,3 +23,16 @@ pip install pyttsx3
 pip install wikipedia
 
 pip install SpeechRecognition
+
+-----------------------
+
+A instalação do FFmpeg pode variar dependendo do sistema operacional que você está utilizando. Aqui estão algumas maneiras comuns de instalar o FFmpeg em diferentes sistemas:
+
+Windows:
+Baixar o FFmpeg Binaries: Você pode baixar os binários do FFmpeg para Windows no site oficial: https://ffmpeg.org/download.html. Baixe o arquivo ZIP correspondente à sua arquitetura (32 ou 64 bits).
+
+Extrair os arquivos: Após baixar o arquivo ZIP, extraia seu conteúdo para um diretório de sua escolha.
+
+Adicionar ao PATH: Para usar o FFmpeg de qualquer lugar no seu sistema, você precisa adicionar o diretório onde os binários do FFmpeg foram extraídos ao PATH do sistema. Você pode fazer isso nas configurações de ambiente do Windows.
+
+winget install "FFmpeg (Essentials Build)"
--- index.py
+++ index.py
@@ -1,9 +1,11 @@
 import speech_recognition as sr
-import pyttsx3
+from gtts import gTTS
+import pygame
+from io import BytesIO
+from pydub import AudioSegment, effects
 
 microfone = sr.Recognizer()
-engine = pyttsx3.init('dummy')
-
+        
 print('EVA iniciada')
 
 try:
@@ -11,31 +13,52 @@ try:
         # microfone.adjust_for_ambient_noise(source)
         print('Escutando...')
         audioCapturado = microfone.listen(source)
-
         comando = microfone.recognize_google(audioCapturado, language='pt-BR')
         comando = comando.lower()
 
         print("Comando capturado: " + comando)
+
+        comando = 'eva Vocês se encontram na entrada da pequena vila de Alderspring, onde a última luz do sol pinta o céu em tons de laranja e púrpura. As primeiras estrelas começam a aparecer, pontilhando o firmamento com promessas de mistérios por descobrir. O vento carrega o cheiro de pão recém-assado, misturado com o aroma da terra molhada da recente chuva de primavera.'
             
         if 'eva' in comando:
             comando = comando.replace('eva', '', 1)
-            engine.say(comando)
-            engine.runAndWait()
+            # Cria um objeto gTTS
+            tts = gTTS(text=comando, lang='pt-br')
+
+            # Salva a saída em um buffer de memória
+            buffer = BytesIO()
+            tts.write_to_fp(buffer)
+            buffer.seek(0)
+
+            # Carrega o buffer de memória como um AudioSegment
+            audio = AudioSegment.from_file(buffer, format="mp3")
+
+            # Ajusta a velocidade do áudio
+            audio = audio.speedup(playback_speed=1.4)
+
+            # Normaliza o áudio
+            audio = effects.normalize(audio)
+
+            # Converte o áudio de volta para o formato de buffer de memória
+            buffer = BytesIO()
+            audio.export(buffer, format="mp3")
+            buffer.seek(0)
+
+            # Inicializa o mixer do pygame
+            pygame.mixer.init()
+
+            # Carrega o buffer de memória como um stream
+            pygame.mixer.music.load(buffer)
+
+            # Reproduz o áudio
+            pygame.mixer.music.play()
+
+            while pygame.mixer.music.get_busy():
+                continue
         
-        if 'erva' in comando:
-            comando = comando.replace('erva', '', 1)
-            engine.say(comando)
-            engine.runAndWait()
-
-        if 'evelyn' in comando:
-            comando = comando.replace('evelyn', '', 1)
-            engine.say(comando)
-            engine.runAndWait()
-
-        if 'ela' in comando:
-            comando = comando.replace('ela', '', 1)
-            engine.say(comando)
-            engine.runAndWait()
+        # if 'erva' in comando:
+        # if 'evelyn' in comando:
+        # if 'ela' in comando:
 
 except Exception as err:
     print('Audio não foi capturado...')
