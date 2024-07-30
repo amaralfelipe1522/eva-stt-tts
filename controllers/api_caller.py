9a4538e2b266097e3606a9e6b4cdee75ecacd2bc
Felipe Amaral <amaral.felipe@live.com>
2024-07-30 10:18:14 -0300

    add beta call to api and adjust display view

---
+++ controllers/api_caller.py
@@ -0,0 +1,11 @@
+import json
+import requests
+
+def get():
+            try:
+                request = requests.get("https://viacep.com.br/ws/08773535/json/")
+                responseJSON = json.loads(request.content)
+                print(responseJSON['localidade'])
+                return responseJSON['localidade']
+            except Exception as e:
+                print(f"Ocorreu um erro na chamada da API: {e}")
