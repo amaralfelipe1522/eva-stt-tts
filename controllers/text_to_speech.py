2a99be322c08c2f3f96f8bb8e7462d7662feb2f4
Felipe Amaral <amaral.felipe@live.com>
2024-08-01 10:58:28 -0300

    included free stt method into error exception

--- controllers/text_to_speech.py
+++ controllers/text_to_speech.py
@@ -2,6 +2,9 @@ import pygame
 import boto3 # type: ignore
 from dotenv import load_dotenv # type: ignore
 import os
+from gtts import gTTS
+from io import BytesIO
+from pydub import AudioSegment, effects
 
 load_dotenv()
 
@@ -39,7 +42,38 @@ class TTSModule:
                 pygame.time.Clock().tick(10)
 
         except Exception as err:
-            print('Não foi possível reproduzir o áudio...')
+            print('text_to_speech.py > Erro no serviço de TTS Polly da AWS')
+            print(err)
+            TTSModuleFREE(comando)
+        finally:
+            pygame.mixer.quit()
+
+class TTSModuleFREE:
+    def __init__(self, comando):
+        try:
+            tts = gTTS(text=comando, lang='pt-br')
+
+            buffer = BytesIO()
+            tts.write_to_fp(buffer)
+            buffer.seek(0)
+
+            audio = AudioSegment.from_file(buffer, format="mp3")
+            audio = audio.speedup(playback_speed=1.3)
+            audio = effects.normalize(audio)
+
+            buffer = BytesIO()
+            audio.export(buffer, format="mp3")
+            buffer.seek(0)
+
+            pygame.mixer.init()
+            pygame.mixer.music.load(buffer)
+            pygame.mixer.music.play()
+
+            while pygame.mixer.music.get_busy():
+                pygame.time.Clock().tick(10)
+
+        except Exception as err:
+            print('text_to_speech.py > Erro no serviço de TTS gTTS da Google')
             print(err)
         finally:
             pygame.mixer.quit()
